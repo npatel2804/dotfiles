862e6231cc4ca0b87a31e7996e2b02e85ee75968
supermario2804 <nikhil2804patel@gmail.com>
2023-11-05 15:38:32 +0530

    updated temper monkey script for habitica.

--- tempermonkey_scripts/habitica/hide_not_today_todos.js
+++ tempermonkey_scripts/habitica/hide_not_today_todos.js
@@ -50,6 +50,30 @@ debugger;
     }
 
 
+    function isDateInPastOrNullOrFalse(inputString) {
+        // Use a regular expression to find a date in the format MM/DD/YYYY
+        const dateRegex = /\d{1,2}\/\d{1,2}\/\d{4}/;
+        const dateMatch = inputString.match(dateRegex);
+        console.log("In dateMatch")
+        console.log(dateMatch)
+
+        if (dateMatch) {
+            // If a date is found, parse it into a JavaScript Date object
+            const date = new Date(dateMatch[0]);
+            const currentDate = new Date();
+
+            if (date < currentDate) {
+                // Date is in the past, return true
+                return true;
+            } else {
+                // Date is in the future, return false
+                return false;
+            }
+        } else {
+            // If no date is found, return null
+            return null;
+        }
+    }
 
     /**
      * Get task with title #pomodoro
@@ -57,18 +81,18 @@ debugger;
      */
     function getPomodoroTask() {
         const habitTasks = Array.from(document.querySelectorAll('.task.type_todo'));
-let taskLoaded = false
+        let taskLoaded = false
         const pomodoroTask = habitTasks.find(task => {
             const text = task.querySelector('.task-content')
-            if (!text.outerText.includes("\nToday")) {
+            if ((!text.outerText.includes("\nToday")) && !isDateInPastOrNullOrFalse(text.outerText)) {
                 task.style.display = 'none';
                 console.log(text.outerText)
             }
             taskLoaded = true
-       return false
+            return false
         });
 
-      return taskLoaded
+        return taskLoaded
 
     }
 
@@ -81,7 +105,7 @@ let taskLoaded = false
             logs('Starting habiticaPomodoro script');
             const pomodoroTask = await waitForExistance(getPomodoroTask);
             //createSoundPlayer(['Todo', 'Chat']);
-           // convertTask(pomodoroTask);
+            // convertTask(pomodoroTask);
         } catch (error) {
             logs('Error on habiticaPomodoro.user.js', { error });
         }
